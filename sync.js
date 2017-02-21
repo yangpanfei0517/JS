77dc72eb54132b7e869edbd77c3ac687d9fef4cd
yangpanfei0517 <yangfeiei@126.com>
2017-02-21 11:11:11 +0800

    同步方法例子

---
+++ sync.js
@@ -0,0 +1,13 @@
+var http = require('http');
+var fs = require('fs');
+http.createServer(function(req,res){
+    if(req.url == '/sync'){
+        fs.createReadStream('sync.html').pipe(res);
+    }else if(req.url == '/ajax'){
+        setTimeout(function(){
+            res.end('ok');
+        },5000);
+    }else{
+        res.end('Not Found');
+    }
+}).listen(8080);
