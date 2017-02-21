dcb3110da11a471dbf3cd5861b2aa2c6e44bb193
yangpanfei0517 <yangfeiei@126.com>
2017-02-21 11:53:30 +0800

    异步执行

--- sync.js
+++ sync.js
@@ -3,6 +3,8 @@ var fs = require('fs');
 http.createServer(function(req,res){
     if(req.url == '/sync'){
         fs.createReadStream('sync.html').pipe(res);
+    }else if(req.url == '/async'){
+        fs.createReadStream('async.html').pipe(res);
     }else if(req.url == '/ajax'){
         setTimeout(function(){
             res.end('ok');
